7df3162b27c554ae5d778894f374539bed0a2448
BarnalaManognaram <bmanognaram@gmail.com>
2026-05-26 14:13:18 +0530

    exam section commit

--- StudentManagementSystem/javascript/home.js
+++ StudentManagementSystem/javascript/home.js
@@ -167,3 +167,19 @@ window.addEventListener(
     "DOMContentLoaded",
     onload
 );
+
+window.addEventListener("load", () => {
+
+  const navEntries =
+    performance.getEntriesByType("navigation");
+
+  if (
+    navEntries.length > 0 &&
+    navEntries[0].type === "reload"
+  ) {
+
+    window.location.href = "index.html";
+
+  }
+
+});
